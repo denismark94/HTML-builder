531ff61fd9eed78d00ac7f518e5fe8f9eb565ca7
denismark94 <denismark94@gmail.com>
2022-05-24 01:01:58 +0300

    feat: add 04-copy-directory & 05-merge-styles tasks solutions

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
 
+async function copyDir(src) {
+  let dst = src.replace('files','files-copy');
+  if (src === path.join(__dirname,'files')) {
+    await fsPromises.rmdir(dst,{recursive:true, force:true});
+  }
+  fsPromises.mkdir(dst,{recursive:true});
+  fs.readdir(src,{withFileTypes:true},(err, files)=>{
+    files.forEach(file=>{
+      if (file.isDirectory()) {
+        fsPromises.mkdir(path.join(src,file.name).replace('files','files-copy'),{recursive:true});
+        copyDir(path.join(src,file.name));
+      }
+      else {
+        fsPromises.copyFile(path.join(src,file.name), path.join(src,file.name).replace('files','files-copy'),fs.constants.COPYFILE_FICLONE);
+      }
+    });
+  });
+}
+
+copyDir(path.join(__dirname,'files'));
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,23 @@
+const fsPromises = require('fs/promises');
+const fs = require('fs');
+const path = require('path');
+const bundle = path.join(__dirname,'project-dist','bundle.css');
+
+async function update(src) {
+  if (src === path.join(__dirname,'styles')) await fs.rm(bundle,(err)=>{if (err) throw err;});
+  let files = await fsPromises.readdir(src,{withFileTypes:true});
+  files.forEach(file=>{
+    if (file.isDirectory()) {
+      update(path.join(src,file.name));
+    }
+    else {
+      if (path.extname(file.name) === '.css') {
+        fs.readFile(path.join(src, file.name),(err, data)=>{
+          fsPromises.appendFile(bundle, data);
+        });
+      }
+    }
+  });
+}
+
+update(path.join(__dirname,'styles'));
